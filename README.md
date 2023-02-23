d12fc7ec3a8a51dd0e646ffde45fda1d1fba4cc2
Vaghela Sagar <vaghela-sagar@Vaghelas-MacBook-Pro.local>
2023-02-23 12:40:52 +0530

    added console log and readme file

---
+++ README.md
@@ -0,0 +1,3 @@
+# jack-library-combiner
+
+
--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import { ThirdwebSDK } from "@thirdweb-dev/sdk";
 import { ethers } from 'ethers';
 
 // process.env.multiChainRegistryAddress = false;
-// Use the Thirdweb and ethers libraries here
 
-console.log('ThirdwebSDK => Inside Index.Js file =>', ThirdwebSDK);
-console.log('ethers => Inside Index.Js file =>', ethers);
+// Use the Thirdweb and ethers libraries here
+console.log('ThirdwebSDK => inside index.js file =>', ThirdwebSDK);
+console.log('ethers => inside index.js file =>', ethers);
 
 window.ThirdwebSDK = ThirdwebSDK;
 window.ethers = ethers;
